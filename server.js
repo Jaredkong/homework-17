8aa8fadef20a73399442b55fd1b965c9c2546029
Jared Kong <jlkong001@yahoo.com>
2020-05-22 01:09:55 -0700

    Yay

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require(`express`);
+const logger = require(`morgan`);
+const mongoose = require(`mongoose`);
+const htmlR = require(`./routes/views`);
+const apiR = require(`./routes/api`);
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger(`dev`));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(`public`));
+
+
+htmlR(app);
+apiR(app);
+
+
+mongoose.connect(`mongodb://localhost/workout`, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
