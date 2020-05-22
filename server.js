40ca77bae48d14185a525206462e1e42cdb22573
Jared Kong <jlkong001@yahoo.com>
2020-05-22 01:20:32 -0700

    added mongo, changed readme

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ htmlR(app);
 apiR(app);
 
 
-mongoose.connect(`mongodb://localhost/workout`, {
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/workout`, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
